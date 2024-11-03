45be5b9f205eb4aa7c8b7b3137c2b5638875ef79
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-11-03 15:07:16 +0500

    fixed: cors

--- m-look-back/main.py
+++ m-look-back/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
 from api.auth import auth_router
 from database.session import create_tables
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",   
+    "https://yourdomain.com",  
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,6 +24,14 @@ app = FastAPI(
     docs_url="/api/docs",  
     redoc_url="/api/redoc",  
     openapi_url="/api/openapi.json", 
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],       
+    allow_headers=["*"],       
 )
 
 @app.get("/api")
