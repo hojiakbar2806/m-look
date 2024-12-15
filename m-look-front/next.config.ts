ce60bb197f39dd46ddc773c7a4607b15a98bc07d
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-12-15 19:31:19 +0500

    fix

--- m-look-front/next.config.ts
+++ m-look-front/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  basePath: process.env.NODE_ENV === "production" ? "/m-look" : "",
+  basePath: process.env.NODE_ENV !== "production" ? "/m-look" : "",
   images: {
     remotePatterns: [
       {
